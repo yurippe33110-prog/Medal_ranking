da09250c7e02088da7569bc7ab92c6ae52be262a
yurippe33110-prog <yurippe33110@gmail.com>
2026-02-20 01:39:49 +0000

    Olympic Madal Ranking

--- Ranking.py
+++ Ranking.py
@@ -1,8 +1,17 @@
 def get_medal(score):
+
+    """Determines the Olympic medal. Includes logic for word record detection and input validation. """
+
+    WORLD_RECORED = 158
+    GOLD = 152
+    SILVER = 145
+    Bronze = 142
+
+
     if score >=158:
         return "Gold Medal! Also, word record! Congratulations!"
     elif score >=152:
-        return "Glod Medal!"
+        return "Gold Medal!"
     elif score >=145:
         return "Silver Medal!"
     elif score >= 142:
@@ -10,11 +19,11 @@ def get_medal(score):
     else:
         return "No medal."
     
-
-try:
-    user_input = input ("What is the athlete's score?")
-    score = float(user_input)
-    result = get_medal(score)
-    print(f"Result: {result}")
-except:
-    print("Error: Please enter a valid number.")
+if __name__ == "__main__":
+    try:
+        user_input = input ("What is the athlete's score?")
+        score = float(user_input)
+        result = get_medal(score)
+        print(f"Result: {result}")
+    except ValueError:
+        print("Error: Please enter a valid number.")
